e7e99d9b8e33b19ab31e4e376ce9b04a6f86dbe8
Jonathan Sudibya <jonathans121@gmail.com>
2015-09-17 12:28:30 +0700

    update conflict

---
+++ mini_mirc_server/src/mini_mirc_server/miniIRCHandler.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mini_mirc_server;
+
+import org.apache.thrift.TException;
+import mini_mirc_server.miniIRC;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class miniIRCHandler implements miniIRC.Iface {
+
+    @Override
+    public int join(String username, String channelname) throws TException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int regUser(String username) throws TException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int leave(String username, String channelname) throws TException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int exit(String username) throws TException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int message(String username, String channelname, String msg) throws TException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String regularUpdate(String username) throws TException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
