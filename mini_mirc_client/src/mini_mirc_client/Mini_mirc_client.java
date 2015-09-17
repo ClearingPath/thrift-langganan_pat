1bf73f2ac55bc363eba2c70df034acfc5d963708
felicia-christie <waxrain1217@gmail.com>
2015-09-17 14:55:38 +0700

    Test connection

--- mini_mirc_client/src/mini_mirc_client/Mini_mirc_client.java
+++ mini_mirc_client/src/mini_mirc_client/Mini_mirc_client.java
@@ -9,13 +9,45 @@ package mini_mirc_client;
  *
  * @author Jonathan
  */
+
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import mini_mirc_client.miniIRC;
+
 public class Mini_mirc_client {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+	try{
+	    TTransport transport;
+	    transport = new TSocket("localhost", 2121);
+	    transport.open();
+	    
+	    TProtocol protocol = new TBinaryProtocol(transport);
+	    miniIRC.Client client = new miniIRC.Client (protocol);
+	    
+	    perform(client);
+	    
+	    transport.close();
+	    
+	} catch (Exception E){
+	    E.printStackTrace();
+	}
     }
+    
+    private static void perform (miniIRC.Client client ) throws TException {
+	String uname = "sampleuser";
+	client.regUser(uname);
+	System.out.println("Registered user: " + uname);
+    }
+    
+    
+    
     
 }
