fe3c1e43c7446474964ef96536dba7583dabd2d5
felicia-christie <waxrain1217@gmail.com>
2015-09-18 11:00:26 +0700

    updet client

--- mini_mirc_client/src/mini_mirc_client/Mini_mirc_client.java
+++ mini_mirc_client/src/mini_mirc_client/Mini_mirc_client.java
@@ -10,6 +10,7 @@ package mini_mirc_client;
  * @author Jonathan
  */
 
+import java.util.Scanner;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
@@ -41,10 +42,49 @@ public class Mini_mirc_client {
 	}
     }
     
-    private static void perform (miniIRC.Client client ) throws TException {
-	String uname = "sampleuser";
-	client.regUser(uname);
-	System.out.println("Registered user: " + uname);
+    private static void perform (miniIRC.Client client) throws TException {
+	boolean exit = false;
+	Scanner input = new Scanner(System.in);
+	String command = input.nextLine();
+	String username = "";
+	
+	while (!exit){
+	    //parse command
+	    String resSplit[] = command.split(" ", 2);
+    	    String commandWord = resSplit[0].toUpperCase();
+	    switch (commandWord){
+		case "/NICK":
+		    System.out.println("Status: Registering user: " + resSplit[1]);
+		    if (client.regUser(resSplit[1]) == 0){
+			System.out.println("Status: Registered user: " + resSplit[1]);
+			username = resSplit[1];
+		    }
+		    else {
+			System.out.println("Error: Unidentified error on register!");
+			
+		    }
+		    break;
+		case "/JOIN":
+		    System.out.println("Status: Checking  channel: " + resSplit[1]);
+		    int res = client.join(username, resSplit[1]);
+		    if (res == 0){
+			System.out.println("");
+		    } else {
+			if (res == 1){
+			    System.out.println("Error: Channel " + resSplit[1] + " already joined!");
+			}
+		    }
+		    
+		    break;
+		case "/LEAVE":
+		    break;
+		case "/EXIT":
+		    exit = true;
+		    break;
+		    
+	    }
+	    
+	}
     }
     
     
