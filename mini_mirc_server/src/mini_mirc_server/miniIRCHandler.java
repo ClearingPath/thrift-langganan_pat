401ad51c9d3a31dc8849f9d9aee48432a6f6f84d
Jonathan Sudibya <jonathans121@gmail.com>
2015-09-18 10:59:34 +0700

    implemented : join, reguser, and leave

--- mini_mirc_server/src/mini_mirc_server/miniIRCHandler.java
+++ mini_mirc_server/src/mini_mirc_server/miniIRCHandler.java
@@ -5,6 +5,11 @@
  */
 package mini_mirc_server;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
 import org.apache.thrift.TException;
 
 /**
@@ -15,14 +20,12 @@ public class miniIRCHandler implements miniIRC.Iface {
 
     @Override
     public int join(String username, String channelname) throws TException {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        return 0;
+        return AddChannel(username,channelname);
     }
 
     @Override
     public int regUser(String username) throws TException {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        return 0;
+        return AddUser(username);
     }
 
     @Override
@@ -50,4 +53,58 @@ public class miniIRCHandler implements miniIRC.Iface {
         return ret;
     }
     
+    private int AddChannel(String Username, String Channel){
+        int ret = 0;
+        MongoClient mongoClient = new MongoClient();
+        DB db = mongoClient.getDB( "mirc" );
+        DBCollection coll = db.getCollection("channelCollection");
+        BasicDBObject query = new BasicDBObject("username", Username)
+                                        .append("channel", Channel);
+
+        DBCursor cursor = coll.find(query);
+
+        try {
+           if(cursor.hasNext()) {
+               ret = 1;
+               System.err.println(Username + " has joined Channel : " + Channel + "!");
+           }
+           else {
+               BasicDBObject doc = new BasicDBObject ("username",Username)
+                                              .append("Channel", Channel);
+               coll.insert(doc);
+               //Extend : Give message if new channel created
+               System.out.println(Username + " joined Channel : " + Channel);
+           }
+        } finally {
+           cursor.close();
+        }
+        return ret;
+    }
+    
+    private int AddUser (String username){
+        int ret = 0;
+        MongoClient mongoClient = new MongoClient();
+        DB db = mongoClient.getDB( "mirc" );
+        DBCollection coll = db.getCollection("activeUser");
+        BasicDBObject query = new BasicDBObject("username", username);
+        
+        DBCursor cursor = coll.find(query);
+        
+        try {
+           if(cursor.hasNext()) {
+               ret = 1;
+               System.err.println(username + " has been used !");
+           }
+           else {
+               java.util.Date date= new java.util.Date();
+               BasicDBObject doc = new BasicDBObject ("username", username)
+                                              .append("timestamp", date);
+               coll.insert(doc);
+               System.out.println(username + " online !");
+           }
+        } finally {
+           cursor.close();
+        }
+        return ret;
+    }
 }
